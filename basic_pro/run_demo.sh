c8e30666aebd9b5012dc02dde81e4e6fde59d1e4
jiangjinhao <jiangjinhao@vip.qq.com>
2017-04-10 21:50:05 +0800

    为了切换分支

--- basic_pro/run_demo.sh
+++ basic_pro/run_demo.sh
@@ -22,9 +22,5 @@ if [ ! -d "$figdir" ]; then
 fi
 
 label="10s"
-#bw=100
-./tcp_fair_RED.sh  30  ${label}  RED1tab
-./tcp_fair_RED.sh  50  ${label}  RED1tab
-./tcp_fair_RED.sh  100  ${label}  RED1tab
-./tcp_fair_RED.sh  200  ${label}  RED1tab
-./tcp_fair_RED.sh  500  ${label}  RED1tab
+bw=100
+./tcp_fair_RED.sh  ${bw}  ${label}  RED1tab
